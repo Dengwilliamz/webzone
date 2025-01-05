593b5f932b436458461dda2e719f7853f7e45204
Dengwilliamz <dengwuor36@gmail.com>
2025-01-05 15:27:19 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,17 @@
+<div class="header">
+    <div class="logo"><a href="index.php"><img src="img\dstlogo.png" alt="" height="80px"></a></div>
+    <div class="main">
+
+    <div class="content">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+        </ul>
+        <div class="user"><a href="#">Deng</a></div>
+    </div>
+    </div>
+
+    
+</div>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>practis website</title>
+    <link rel="shortcut icon" href="img\dstlogo.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/header.css">
+</head>
+<body>
+    <div>
+        <?php include 'header.php';?>
+    </div>
+    <div class="container">
+        <input type="text" name="" id="" placeholder="Enter Email">
+        <input type="password" name="" id="" placeholder="password">
+        <button type="submit">Login</button>
+    </div>
+    
+</body>
+</html>
